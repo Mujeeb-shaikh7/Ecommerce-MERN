10357e9ffdea8e64c22553eca41638ffdcbba045
Shaik Mujeeb <shaikrahaman777@gmail.com>
2024-02-02 12:20:42 +0530

    axios api requests

--- client/src/components/layouts/Layout.js
+++ client/src/components/layouts/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import {Helmet} from "react-helmet";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = ({children,title,description,keywords,author}) => {
   return (
@@ -12,10 +14,14 @@ const Layout = ({children,title,description,keywords,author}) => {
   <meta name="description" content={description} />
   <meta name="keywords" content={keywords} />
   <meta name="author" content={author} />
+
   <title>{title}</title>
     </Helmet>
         <Header />
-        <main style={{minHeight:"70vh"}}>{children}</main>
+        <main style={{minHeight:"70vh"}}>
+        <ToastContainer />
+          {children}
+          </main>
         <Footer />
     </div>
   )
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,42 +1,63 @@
 import React, { useState } from 'react'
 import Layout from '../../components/layouts/Layout'
-
+import {toast} from 'react-toastify'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 const Register = () => {
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     const [phone,setPhone] = useState('');
     const [address,setAddress] = useState('');
+    const navigate=useNavigate();
+    let handleInput=async(e)=>{
+        e.preventDefault();
+        try {
+            const res=await axios.post(
+                '/api/v1/auth/register',
+                {name,email,password,phone,address}
+            );
+            if(res.data.success){
+                toast.success(res.data.message);
+                navigate('/login')
+            }else{
+                toast.error(res.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something went wrong")
+        }
+    }
     return (
         <Layout title={"Register page"}>
             <div className='register'>
                 <h1>Register</h1>
-                <form>
+                <form onSubmit={handleInput}>
                     <div className="mb-3">
                         
-                        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="name" placeholder='name' />
+                        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} required className="form-control" id="name" placeholder='name' />
                         
                     </div>
                     <div className="mb-3">
                         
-                        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" id="email" placeholder='email' />
+                        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required className="form-control" id="email" placeholder='email' />
                         
                     </div>
                     <div className="mb-3">
     
-                        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" id="password" placeholder='password' />
+                        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required className="form-control" id="password" placeholder='password' />
                     </div>
                     <div className="mb-3">
                         
-                        <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} className="form-control" id="phone" placeholder='phone' maxLength={10} />
+                        <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} required className="form-control" id="phone" placeholder='phone' maxLength={10} />
                         
                     </div>
                     <div className="mb-3">
                         
-                        <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} className="form-control" id="address" placeholder='address' />
+                        <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} required className="form-control" id="address" placeholder='address' />
                         
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit"  className="btn btn-primary">Submit</button>
                 </form>
 
             </div>
